c42dffa5701d055ba03abe41137440e86968cd7e
yerayM21 <111537718+yerayM21@users.noreply.github.com>
2023-12-29 22:02:02 -0500

    agregar barra de deplazamiento

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.') 
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
